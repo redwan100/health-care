5a82a2c624a4feda3e2640027329d61ac27abced
Redwan islam <redwanislam2760@gmail.com>
2024-03-23 23:41:12 +0600

    added role based access functionality

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,18 +1,37 @@
+import { UserRole } from "@prisma/client";
 import { Router } from "express";
 import validateRequest from "../../../shared/validateRequest";
+import auth from "../../middlewares/auth";
 import { AdminController } from "./admin.controller";
 import { AdminValidation } from "./admin.validation";
 
 const router = Router();
 
-router.get("/", AdminController.getAllAdmin);
-router.get("/:adminId", AdminController.getSingleAdmin);
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getAllAdmin
+);
+router.get(
+  "/:adminId",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.getSingleAdmin
+);
 router.patch(
   "/:adminId",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(AdminValidation.updateValidationSchema),
   AdminController.updateAdmin
 );
-router.delete("/:adminId", AdminController.deleteAdmin);
-router.delete("/soft/:adminId", AdminController.softDeleteAdmin);
+router.delete(
+  "/:adminId",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.deleteAdmin
+);
+router.delete(
+  "/soft/:adminId",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminController.softDeleteAdmin
+);
 
 export const AdminRoute = router;
